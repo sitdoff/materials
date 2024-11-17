667af405f3afb5a04729e983df6741fb5de423de
Roman Ivanov <sitdoff@gmail.com>
2024-11-17 15:10:47 +0300

    update entrypoint.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,17 @@ set -e
 ./wait-for-it.sh postgres_db:5432 -- echo "PostgreSQL is ready"
 
 # Применение миграций, если это необходимо
-# python manage.py makemigrations && python manage.py migrate
+python manage.py makemigrations && python manage.py migrate
+
+# Заполняем базу начальными данными
+python manage.py loaddata dump.json
+
+# Вносим в базу суперюзера
+python manage.py loaddata superuser.json
+
+# Собираем статику
+python manage.py collectstatic --noinput
+
 
 # Запуск вашего Django приложения
 exec "$@"
