4e8ceacc23d162cf712f11aedac4e5d255d81499
Roman Ivanov <sitdoff@gmail.com>
2024-11-20 11:16:39 +0300

    update category_service

--- core/api/v1/catalog/services/category_service.py
+++ core/api/v1/catalog/services/category_service.py
@@ -13,6 +13,33 @@ class CategoryService(CategoryServiceBase):
     def tree_categories(self):
         return self.repository.tree()
 
+    def build_category_tree(self, categories):
+        category_dict = {category.id: category for category in categories}
+        tree = []
+
+        for category in categories:
+            if category.parent_id:
+                parent = category_dict[category.parent_id]
+                if not hasattr(parent, "children_list"):
+                    parent.children_list = []
+                parent.children_list.append(category)
+            else:
+                tree.append(category)
+
+        return tree
+
+    def set_tree_total_prices(self, queryset):
+        queryset_total_price = 0
+        for category in queryset:
+            if not hasattr(category, "children_list"):
+                queryset_total_price += category.total_price
+            else:
+                category_total_price = self.set_tree_total_prices(category.children_list)
+                category.total_price = category_total_price
+                queryset_total_price += category_total_price
+
+        return queryset_total_price
+
     def get_category_by_id(self, target_id):
         return self.repository.get_by_id(target_id)
 
