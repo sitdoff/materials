9b60968626572e67b1fd41de04811773a048d380
Roman Ivanov <sitdoff@gmail.com>
2024-11-20 11:17:33 +0300

    update document_service.py

--- core/api/v1/catalog/services/document_service.py
+++ core/api/v1/catalog/services/document_service.py
@@ -4,11 +4,16 @@ from .base_service import DocumentServiceBase
 
 
 class DocumentService(DocumentServiceBase):
-    def save_document(self, data):
+    def upload_document(self, data):
         document_data = self.repository.save_file(data)
-        document = self.repository.save_file_in_db(**document_data)
-        import_materials_from_xls.delay_on_commit(document.pk)
+        return document_data
+
+    def save(self, data):
+        document = self.repository.save_file_in_db(**data)
         return document
+
+    def run_task(self, document_id):
+        import_materials_from_xls.delay_on_commit(document_id)
 
     def get_document(self, target_id):
         return self.repository.get_document(target_id)
