0ca155fa0ed774165e1f8778ace4e6faec3baae4
Roman Ivanov <sitdoff@gmail.com>
2024-11-25 12:07:04 +0300

    update DocumentSerializer

--- core/apps/catalog/serializers/document.py
+++ core/apps/catalog/serializers/document.py
@@ -5,9 +5,8 @@ from core.apps.common.serializers import BaseModelSerializer
 
 
 class DocumentSerializer(BaseModelSerializer):
-    file_path = serializers.CharField(write_only=True)
     status = serializers.CharField(read_only=True)
 
     class Meta:
         model = DocumentModel
-        fields = ["id", "title", "file_path", "status", "created_at"]
+        fields = ["id", "file", "status", "created_at"]
